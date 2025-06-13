937730db073af0eb97a91b0bb628b237af218288
Ojas S <ojas1807@gmail.com>
2025-06-13 14:29:52 -0400

    Added pictures

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,18 @@
 import Image from "next/image";
 import Navbar from "../components/ui/navbar";
 import { Button } from "@/components/ui/button";
+import DraggableImages from "@/components/ui/draggable-images";
 
 export default function Home() {
   return (
     <>
+      <div className="z-50 fixed">
+        <DraggableImages />
+      </div>
       <Navbar />
       <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
         <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-          <h1 className="text-4xl font-bold">Welcome</h1>
+          <h1 className="text-4xl font-bold">Welcome I'm avan ;)</h1>
 
           <div className="flex gap-4 items-center flex-col sm:flex-row"></div>
         </main>
