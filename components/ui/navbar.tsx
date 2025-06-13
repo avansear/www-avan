18985c6d0f4f544c3b1cf73907898d96ff2e5391
Ojas S <ojas1807@gmail.com>
2025-06-13 01:25:36 -0400

    Added some other things

--- components/ui/navbar.tsx
+++ components/ui/navbar.tsx
@@ -3,10 +3,9 @@ import Link from 'next/link';
 export default function Navbar() {
   return (
     <nav className="w-full flex items-center justify-between py-4 px-8 bg-background border-b">
-      <div className="font-bold text-lg">Logo</div>
+      <div className="font-bold text-lg">avansear</div>
       <div className="flex gap-6">
-        <Link href="/" className="hover:underline">Home</Link>
-        <Link href="/about" className="hover:underline">About</Link>
+        <div>weird high functioning insomniac designer</div>
       </div>
     </nav>
   );
